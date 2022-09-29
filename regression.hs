24cd6bc0f914abead01b8c27b7d72d1c275b3215
whatf0xx <whatf0xx@gmail.com>
2022-09-29 16:59:51 +0200

    More functions. Refactor 'Matrix' to check shape.

--- regression.hs
+++ regression.hs
@@ -1,5 +1,8 @@
 type XYpoint = (Double, Double)
 type XYPoints = [XYpoint]
+type RowVector = [Double]
+type ColumnVector = [Double]
+type Matrix = [RowVector]
 type Polynomial = (Double -> Double)
 
 -- instance Show Polynomial where
@@ -16,3 +19,16 @@ polyFromList [] = error "Empty list passed: no coefficients to define polynomial
 polyFromList [a] = const a
 polyFromList (a:as) = \x -> a + polyFromList (listTimesDouble as x) x
 
+vecInnerProduct :: RowVector -> ColumnVector -> Double
+vecInnerProduct [] [] = error "Both arguments empty."
+vecInnerProduct [] _ = error "Empty row vector passed."
+vecInnerProduct _ [] = error "Empty column vector passed."
+vecInnerProduct [r] [c] = r * c
+vecInnerProduct (r:rs) (c:cs) = r * c + vecInnerProduct rs cs
+
+nthCol :: Matrix -> Int -> ColumnVector
+
+
+listOfPows :: Double -> Int -> RowVector
+listOfPows seed 1 = [1]
+listOfPows seed n = listOfPows seed (n-1) ++ [seed^(n-1)]
