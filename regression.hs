c096f9304e89c6f8d8a7ed61b8f5927cb4f0d83a
whatf0xx <whatf0xx@gmail.com>
2022-09-30 15:16:47 +0200

    Problem set out as coeffs. matrix and vector.

--- regression.hs
+++ regression.hs
@@ -26,9 +26,18 @@ vecInnerProduct _ [] = error "Empty column vector passed."
 vecInnerProduct [r] [c] = r * c
 vecInnerProduct (r:rs) (c:cs) = r * c + vecInnerProduct rs cs
 
-nthCol :: Matrix -> Int -> ColumnVector
-
-
 listOfPows :: Double -> Int -> RowVector
 listOfPows seed 1 = [1]
 listOfPows seed n = listOfPows seed (n-1) ++ [seed^(n-1)]
+
+getMatrix :: XYPoints -> Int -> Matrix
+getMatrix [] _ = []
+getMatrix ((x, _):ts) n = listOfPows x n : getMatrix ts n
+
+getColVec :: XYPoints -> ColumnVector
+getColVec [] = []
+getColVec ((_, y):ts) = y : getColVec ts
+
+setProblem :: XYPoints -> (Matrix, ColumnVector)
+setProblem points = (getMatrix points (length points), getColVec points) -- this can definitely be refactored!
+
